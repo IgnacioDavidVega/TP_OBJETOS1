3b817df616864a2ad56264ca3334ee8ffbd09c52
Ignacio David Vega <ignaciodavidvega@outlook.com>
2025-09-16 15:12:27 -0300

    Añadir clases Equipo y Jugador con atributos y métodos básicos

---
+++ Trabajo_Practico_OO1/src/Sistema_Futbol/Equipo.java
@@ -0,0 +1,12 @@
+package Sistema_Futbol;
+
+import java.time.LocalDate;
+
+
+public class Equipo {
+
+    private int id;
+    private String nombre;
+    private String codUnico;
+    private LocalDate fechaFundacion;
+}
---
+++ Trabajo_Practico_OO1/src/Sistema_Futbol/Jugador.java
@@ -0,0 +1,102 @@
+package Sistema_Futbol;
+
+import java.time.LocalDate;
+
+public class Jugador {
+    // Atributos de la clase Jugador
+    private int id;
+    private String nombre;
+    private String apellido;
+    private long dni;
+    private LocalDate fechaNacimiento;
+    private float estatura;
+    private float peso;
+    private String posicion;
+    private int dorsal;
+
+    // Constructor de la clase Jugador
+    public Jugador(int id, String nombre, String apellido, long dni, LocalDate fechaNacimiento, float estatura, float peso, String posicion, int dorsal) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.posicion = posicion;
+        this.dorsal = dorsal;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+}

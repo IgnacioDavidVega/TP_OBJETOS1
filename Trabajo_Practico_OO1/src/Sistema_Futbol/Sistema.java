18b05c158bf24c4b4640efee5f12937b2ce68daa
Ignacio David Vega <ignaciodavidvega@outlook.com>
2025-10-01 08:30:18 -0300

    Implementacion de entrenadores (#1)
    * Se añade ABM Entrenador.
    Se añade metodo del punto 5 del hito 2.
    
    * Se añade rama.
    
    Se crea ABM para Entrenadores.
    Se añade metodo Punto 5 del hito 2.
    Se añade metodo validarDni.
    
    * Se añade método para comprobar si una persona es mayor de edad.
    
    * Se elimina la clase TestSistema y sus pruebas asociadas.
    
    * Se simplifica la validación del DNI en varios métodos y se corrige el nombre del método de búsqueda de entrenadores. Se añade clase de pruebas para el sistema con casos de uso para agregar, eliminar y modificar entrenadores.
    
    * Se elimina la clase TestSistema y sus pruebas asociadas.
    
    * Se añade la clase TestSistema con pruebas para agregar, eliminar, modificar entrenadores y comprobar la mayoría de edad.
    
    * Se elimina la clase TestSistema y sus pruebas asociadas.

--- Trabajo_Practico_OO1/src/Sistema_Futbol/Sistema.java
+++ Trabajo_Practico_OO1/src/Sistema_Futbol/Sistema.java
@@ -1,5 +1,6 @@
 package Sistema_Futbol;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +11,7 @@ public class Sistema {
 	private List<Torneo> listaTorneo;
 	private List<Partido> listaPartido;
 	private List<Registro> listaRegistro;
-	
+
 	public Sistema() {
 		this.listaEquipos = new ArrayList<Equipo>();
 		this.listaEntrenador = new ArrayList<Entrenador>();
@@ -19,16 +20,15 @@ public class Sistema {
 		this.listaPartido = new ArrayList<Partido>();
 		this.listaRegistro = new ArrayList<Registro>();
 	}
-	
-	
+
 	public void addEquipo(Equipo equipo) {
 		this.listaEquipos.add(equipo);
 	}
-	
+
 	public void addEntrenador(Entrenador entrenador) {
 		this.listaEntrenador.add(entrenador);
 	}
-	
+
 	public void addJugador(Jugador jugador) {
 		this.listaJugador.add(jugador);
 	}
@@ -36,7 +36,7 @@ public class Sistema {
 	public void addTorneo(Torneo torneo) {
 		this.listaTorneo.add(torneo);
 	}
-	
+
 	public void addPartido(Partido partido) {
 		this.listaPartido.add(partido);
 	}
@@ -44,17 +44,17 @@ public class Sistema {
 	public void addRegistro(Registro registro) {
 		this.listaRegistro.add(registro);
 	}
-	
+
 	//
-	
+
 	public void removeEquipo(Equipo equipo) {
 		this.listaEquipos.remove(equipo);
 	}
-	
+
 	public void removeEntrenador(Entrenador entrenador) {
 		this.listaEntrenador.remove(entrenador);
 	}
-	
+
 	public void removeJugador(Jugador jugador) {
 		this.listaJugador.remove(jugador);
 	}
@@ -62,7 +62,7 @@ public class Sistema {
 	public void removeTorneo(Torneo torneo) {
 		this.listaTorneo.remove(torneo);
 	}
-	
+
 	public void removePartido(Partido partido) {
 		this.listaPartido.remove(partido);
 	}
@@ -70,9 +70,9 @@ public class Sistema {
 	public void removeRegistro(Registro registro) {
 		this.listaRegistro.remove(registro);
 	}
-	
+
 	//
-	
+
 	public List<Equipo> getListaEquipos() {
 		return listaEquipos;
 	}
@@ -119,5 +119,158 @@ public class Sistema {
 
 	public void setListaRegistro(List<Registro> listaRegistro) {
 		this.listaRegistro = listaRegistro;
+	}
+
+	// Metodos
+
+	// UTILIDAD VALIDAR DNI DIGITOS
+
+	public boolean validarDni(long dni) throws Exception {
+
+		// Validacion DNI tiene que ser 8 digitos
+		String stringDni = String.valueOf(dni);
+
+		if (stringDni.length() != 8) {
+			throw new Exception("El DNI tiene que tener 8 digitos.");
+		}
+
+		return true;
+	}
+
+	//UTILIDAD COMPROBAR MAYOR DE EDAD
+
+	public boolean comprobarMayorEdad (LocalDate fechaNacimiento){
+
+		boolean mayorEdad;
+		
+		LocalDate fechaActual = LocalDate.now();
+		LocalDate calculoMayorEdad = fechaNacimiento.plusYears(18);
+		
+		if (calculoMayorEdad.isBefore(fechaActual)){
+			mayorEdad = true;
+		}else{
+			mayorEdad = false;
+		}
+
+		return mayorEdad;
+	}
+
+
+	// ENTRENADORES
+	// ALTA ENTRENADOR
+	public boolean agregarEntrenador(String nombre, String apellido, long dni, LocalDate fechaNacimiento,
+			String estrategiaFavorita) throws Exception {
+
+		// Validacion DNI tiene que ser 8 digitos
+		validarDni(dni);
+
+		// Busqueda si existe el entrenador en la BD
+		int contador = 0;
+		boolean existeEntrenador = false;
+
+		while (contador < listaEntrenador.size() && existeEntrenador == false) {
+			if (listaEntrenador.get(contador).getDni() == dni) {
+				existeEntrenador = true;
+			}
+			contador++;
+		}
+
+		if (existeEntrenador == false) {
+			listaEntrenador.add(
+					new Entrenador(listaEntrenador.size() + 1, nombre, apellido, dni, fechaNacimiento,
+							estrategiaFavorita));
+		} else {
+			throw new Exception("El entrenador con DNI " + dni + " ya existe en la BD.");
+		}
+
+		return true;
+	}
+
+	// TRAER ENTRENADOR
+	public Entrenador traerEntrenador(int idEntrenador) throws Exception {
+
+		if (idEntrenador <= 0) {
+			throw new Exception("El ID " + idEntrenador + " que ingreso no puede ser 0 o un numero negativo.");
+		}
+
+		int contador = 0;
+		Entrenador entrenadorEncontrado = null;
+
+		while (entrenadorEncontrado == null && contador < listaEntrenador.size()) {
+			if (listaEntrenador.get(contador).getId() == idEntrenador) {
+				entrenadorEncontrado = listaEntrenador.get(contador);
+			}
+			contador++;
+		}
+
+		if (entrenadorEncontrado == null) {
+			throw new Exception("El ID " + idEntrenador + " que ingreso no existe en la BD.");
+		}
+
+		return entrenadorEncontrado;
+	}
+
+	// BAJA ENTRENADOR
+	public boolean eliminarEntrenadorPorDni(long dni) throws Exception {
+
+		validarDni(dni); // Valida SNI
+
+		int contador = 0;
+		Entrenador entrenadorAEliminar = null;
+
+		while (entrenadorAEliminar == null && contador < listaEntrenador.size()) {
+			if (getListaEntrenador().get(contador).getDni() == dni) {
+				entrenadorAEliminar = getListaEntrenador().get(contador);
+			}
+			contador++;
+		}
+
+		if (entrenadorAEliminar != null) {
+			getListaEntrenador().remove(entrenadorAEliminar);
+		} else {
+			throw new Exception("El entrenador con DNI " + dni + " no existe en la BD. No se pudo eliminar.");
+		}
+
+		return true;
+	}
+
+	public boolean modificarEntrenador(long dni, String nombre, String apellido,
+			LocalDate fechaNacimiento, String estrategiaFavorita) throws Exception {
+
+		validarDni(dni);
+
+		int contador = 0;
+		Entrenador entrenadorEncontrado = null;
+
+		while (entrenadorEncontrado == null && contador < listaEntrenador.size()) {
+			if (listaEntrenador.get(contador).getDni() == dni) {
+				entrenadorEncontrado = listaEntrenador.get(contador);
+			}
+			contador++;
+		}
+
+		if (entrenadorEncontrado != null) {
+			entrenadorEncontrado.setNombre(nombre);
+			entrenadorEncontrado.setApellido(apellido);
+			entrenadorEncontrado.setEstrategiaFavorita(estrategiaFavorita);
+		} else {
+			throw new Exception("MODIFICACION ENTRENADOR: El DNI " + dni + " que ingreso no existe en la BD.");
+		}
+
+		return true;
+	}
+
+	// Búsqueda de entrenadores: Crear un método que, dada una táctica preferida,
+	// devuelva una lista de todos los entrenadores que la utilicen.
+	public List<Entrenador> buscarEntrenador(String estrategia) {
+
+		List<Entrenador> entrenadoresFiltrados = new ArrayList<Entrenador>();
+
+		for (Entrenador entrenador : getListaEntrenador()) {
+			if (entrenador.getEstrategiaFavorita().equals(estrategia)) {
+				entrenadoresFiltrados.add(entrenador);
+			}
+		}
+		return entrenadoresFiltrados;
 	}
 }

532d76d907f5b6b60ecd226b34b155f3d14b6301
Ignacio David Vega <ignaciodavidvega@outlook.com>
2025-10-04 15:10:15 -0300

    Elimina el código de prueba para agregar un jugador en la clase Test.

--- Trabajo_Practico_OO1/src/Test/Test.java
+++ Trabajo_Practico_OO1/src/Test/Test.java
@@ -8,14 +8,5 @@ public class Test {
     public static void main(String[] args) {
 
         Sistema sistema = new Sistema();
-
-        try {
-            sistema.agregarJugador("Ignacio", "Vega", 25, 42366713, LocalDate.of(2000, 2, 25), 170.3f, 55, "Delantero",
-                    11);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(sistema.getListaJugador());
     }
 }

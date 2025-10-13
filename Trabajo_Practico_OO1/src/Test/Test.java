9fef8e45d5a7ee32516976f9509d01f4137e4378
BrianAvila-UNLA <brian.avilaxx03@gmail.com>
2025-10-13 15:50:03 -0300

    Hitos 12 y 13 funcionales
    Funcionan los hitos que trabajan con los registros de partidos de torneo. Tuve que modificar el equals de Partido porque no funcionaba originalmente.

--- Trabajo_Practico_OO1/src/Test/Test.java
+++ Trabajo_Practico_OO1/src/Test/Test.java
@@ -7,6 +7,10 @@ import Sistema_Futbol.*;
 public class Test {
     public static void main(String[] args) {
 
-        Sistema sistema = new Sistema();
+    	 try {
+             Sistema sistema = new Sistema();
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
     }
 }
